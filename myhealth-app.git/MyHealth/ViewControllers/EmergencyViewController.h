7d7e4b02bb73bc8c661ab0142608820365127525
parvind <parvind.bhatt@gmail.com>
2014-12-13 23:06:12 +0530

    again do the code

--- myhealth-app.git/MyHealth/ViewControllers/EmergencyViewController.h
+++ myhealth-app.git/MyHealth/ViewControllers/EmergencyViewController.h
@@ -10,4 +10,17 @@
 #import "UIButton+Extentions.h"
 @interface EmergencyViewController : UIViewController
 @property (nonatomic, retain) IBOutlet UIButton *btn_back;
+@property (weak, nonatomic) IBOutlet UITextField *nameTextField;
+@property (weak, nonatomic) IBOutlet UITextField *addressTextField;
+@property (weak, nonatomic) IBOutlet UITextField *phoneTextField;
+@property (weak, nonatomic) IBOutlet UITextField *ssnTextField;
+@property (weak, nonatomic) IBOutlet UITextField *dobTextField;
+@property (weak, nonatomic) IBOutlet UITextField *emergencyTextField;
+- (IBAction)emergencyBtnClicked:(UIButton *)sender;
+@property (weak, nonatomic) IBOutlet UIButton *nameButton;
+@property (weak, nonatomic) IBOutlet UIButton *addressButton;
+@property (weak, nonatomic) IBOutlet UIButton *phoneButton;
+@property (weak, nonatomic) IBOutlet UIButton *ssnButton;
+@property (weak, nonatomic) IBOutlet UIButton *dobButton;
+@property (weak, nonatomic) IBOutlet UIButton *emergencyButton;
 @end
